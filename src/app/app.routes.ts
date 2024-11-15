be0ea554be2a388624894f96e573951ebe0c2491
Cristina <crisbursanchez@gmail.com>
2024-11-15 15:07:47 +0100

    Formulario login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,10 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-
-export const routes: Routes = [
-
-    { path: '', component: LoginComponent }
-
-=======
 import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
-    { path: '', component: PageHomeComponent }
->>>>>>> bbe599c011b450826cbe6b9361c9203a174b5239
+    { path: 'home', component: PageHomeComponent },
+    { path: 'login', component: LoginComponent }
 ];
