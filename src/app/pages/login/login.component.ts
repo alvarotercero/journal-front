c3fae0bbd777acbd99772f8ca3c1fa06588a1040
Cristina <crisbursanchez@gmail.com>
2024-11-16 12:23:03 +0100

    login terminado

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+
+
+
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [])
+    }, [])
+  }
+
+  getDataLogin() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value)
+    } else {
+      console.log("el formulario no es valido");
+    }
+
+
+  }
 }
