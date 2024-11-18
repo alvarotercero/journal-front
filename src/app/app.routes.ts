f4734e1140ae27ceadd9071f8395865559839259
Cristina <crisbursanchez@gmail.com>
2024-11-18 09:12:10 +0100

    trabajando en html y css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,21 @@ import { PageHomeComponent } from './pages/page-home/page-home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NoticiasDashboardComponent } from './pages/dashboard/noticias-dashboard/noticias-dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PageEdicionComponent } from './pages/dashboard/page-edicion/page-edicion.component';
 
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', component: PageHomeComponent },
     { path: 'login', component: LoginComponent },
+
+
     {
         path: 'dashboard', component: DashboardComponent, children: [
             { path: '', pathMatch: 'full', redirectTo: 'noticias' },
-            { path: 'noticias', component: NoticiasDashboardComponent }
+            { path: 'noticias', component: NoticiasDashboardComponent },
             // { path: 'notcia/:noticiaId', com}
+            { path: 'edicion/:noticiaId', component: PageEdicionComponent },
         ]
     }
 ];
---
+++ src/app/pages/dashboard/page-edicion/page-edicion.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { INoticia } from '../../../interfaces/inoticia.interface';
+
+@Component({
+  selector: 'app-page-edicion',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-edicion.component.html',
+  styleUrl: './page-edicion.component.css'
+})
+export class PageEdicionComponent {
+
+  // contenido: string = '';
+
+
+  // noticia: INoticia = {
+  //   id: 11,
+  //   titular: "La inteligencia artificial llega al cine",
+  //   imagen: "https://example.com/images/ia-cine.jpg",
+  //   texto: "La inteligencia artificial está siendo utilizada para crear guiones y efectos especiales en la industria cinematográfica.",
+  //   secciones: "general",
+  //   fecha_publicacion: "2024-11-10",
+  //   redactor_id: "R011",
+  //   editor_id: "E011",
+  //   categoria_id: "C011",
+  //   estado: "Publicado",
+  //   importancia: "Media",
+  //   cambios: new Date("2024-11-05"),
+  //   slug: "ia-llega-al-cine"
+  // }
+
+
+  // estadoMensaje: string = 'Estado: Sin cambios';
+
+
+
+
+
+}
+
+
+
+
+
+
