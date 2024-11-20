c86d1eb8e402a905fe055b879b689b5b284cd6b0
SimplementeROY <sherwinagunias11@gmail.com>
2024-11-20 10:31:10 +0100

    Arreglando diseño

--- src/app/components/noticia-slider-item/noticia-slider-item.component.ts
+++ src/app/components/noticia-slider-item/noticia-slider-item.component.ts
@@ -1,14 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { INoticia } from '../../interfaces/inoticia.interface';
+import { RouterLink } from '@angular/router';
+import { ICategoria } from '../../interfaces/icategoria.interface';
+import { CategoriasService } from '../../services/categorias.service';
 
 @Component({
   selector: 'app-noticia-slider-item',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './noticia-slider-item.component.html',
   styleUrl: './noticia-slider-item.component.css'
 })
 export class NoticiaSliderItemComponent {
   @Input() miNoticia?: INoticia;
+  categoria?: ICategoria;
+  categoriasService = inject(CategoriasService)
 
+  async ngOnInit() {
+    try {
+      this.categoria = await this.categoriasService.getById(this.miNoticia?.categoria_id)
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
 }
