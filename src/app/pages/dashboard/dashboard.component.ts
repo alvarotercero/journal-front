69785207996cf2520f85fa031ecf505a56ae490c
Fernando <98433286+ferguez22@users.noreply.github.com>
2024-11-20 10:30:11 +0100

    Dashboard en lista

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,30 +1,15 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NoticiasService } from '../../services/noticias.service';
-import { INoticia } from '../../interfaces/inoticia.interface';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FormsModule, RouterModule, CommonModule],
+  imports: [RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 
 export class DashboardComponent {
-
-  noticiasService = inject(NoticiasService)
-  arrNoticias: INoticia[] = []
-
-
-  async ngOnInit() {
-    this.arrNoticias = await this.noticiasService.getByUser(1)
-  }
-
-
-
-
 }
 
