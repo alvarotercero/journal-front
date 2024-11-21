8fd94db0a2b99202d91d3f72d665609a73e4ba86
Fernando <98433286+ferguez22@users.noreply.github.com>
2024-11-21 19:19:52 +0100

    Cambios

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,30 +2,23 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UsuariosService } from '../../services/usuarios.service';
 import { IUsuario } from '../../interfaces/iusuario.interface';
-import { UpperCasePipe } from '@angular/common';
-
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterModule, UpperCasePipe],
+  imports: [RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 
 export class DashboardComponent {
-
-  usuariosService = inject(UsuariosService)
-
-  usuarioLogueado!: IUsuario
-
-  token: string | null = ''
+  usuariosService = inject(UsuariosService);
+  usuarioLogueado: IUsuario | null = null;
+  token: string | null = localStorage.getItem('token');
 
   async ngDoCheck() {
-    this.token = localStorage.getItem('token');
     if (this.token) {
       this.usuarioLogueado = await this.usuariosService.getUsuarioPorId();
     }
   }
 }
-
