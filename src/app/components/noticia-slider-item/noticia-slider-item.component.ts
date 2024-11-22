a2b82508aa77bd6ede521b84f55b7459e8a9bc63
SimplementeROY <sherwinagunias11@gmail.com>
2024-11-22 12:48:09 +0100

    A

--- src/app/components/noticia-slider-item/noticia-slider-item.component.ts
+++ src/app/components/noticia-slider-item/noticia-slider-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { INoticia } from '../../interfaces/inoticia.interface';
 import { RouterLink } from '@angular/router';
+import { RecortarTextoPipe } from '../../pipes/recortar-texto.pipe';
 
 @Component({
   selector: 'app-noticia-slider-item',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RecortarTextoPipe],
   templateUrl: './noticia-slider-item.component.html',
   styleUrl: './noticia-slider-item.component.css'
 })
